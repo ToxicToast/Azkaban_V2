12b6cf699f17ec7f50caf82b21c7f24c92542e44
Thomas Kortyka <t.kortyka@gmx.de>
2023-11-22 00:48:45 +0100

    chore: Rewrite main.ts

--- apps/gateway-service/src/main.ts
+++ apps/gateway-service/src/main.ts
@@ -3,20 +3,49 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
+import { INestApplication, Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function createApp(): Promise<INestApplication> {
+  return await NestFactory.create(AppModule, {
+    snapshot: true,
+  });
+}
+
+function configureApp(app: INestApplication): void {
+  const globalPrefix = 'api';
+  //
+  app.setGlobalPrefix(globalPrefix);
+}
+
+function configureSwagger(app: INestApplication): void {
+  const config = new DocumentBuilder()
+    .setTitle('Azkaban')
+    .setDescription('Home Microservices')
+    .setVersion('0.0.1')
+    .addBearerAuth()
+    .addOAuth2()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  //
+  SwaggerModule.setup('swagger', app, document);
+}
+
+async function startApp(app: INestApplication): Promise<void> {
+  const port = process.env.PORT || 3000;
+  //
+  await app.listen(port);
+}
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
+  const app = await createApp();
+  configureApp(app);
+  configureSwagger(app);
+  await startApp(app);
+  Logger.log(`🚀 Azkaban-Gateway is running`);
 }
 
 bootstrap();
