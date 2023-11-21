55825f622125077b89fb59beb0ac7ed10d038496
Thomas Kortyka <t.kortyka@gmx.de>
2023-11-22 00:49:14 +0100

    feat: add health.module, websockets.module and use router module

--- apps/gateway-service/src/app/app.module.ts
+++ apps/gateway-service/src/app/app.module.ts
@@ -2,9 +2,28 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RouterModule } from '@nestjs/core';
+import { HealthModule } from './health/health.module';
+import { WebsocketsModule } from './websockets/websockets.module';
+import { DevtoolsModule } from '@nestjs/devtools-integration';
 
 @Module({
-  imports: [],
+  imports: [
+    DevtoolsModule.register({
+      http: true,
+      port: 3001,
+    }),
+    //
+    HealthModule,
+    WebsocketsModule,
+    //
+    RouterModule.register([
+      {
+        path: 'health',
+        module: HealthModule,
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
