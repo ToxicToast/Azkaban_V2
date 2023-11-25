975b8f21f2cba81bd7a3eac26187dd7d64c706b7
Thomas Kortyka <t.kortyka@gmx.de>
2023-11-25 02:37:38 +0100

    chore: Remove Category Service and Controller for now

--- apps/gateway-service/src/app/inventory/category/category.module.ts
+++ apps/gateway-service/src/app/inventory/category/category.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { CategoryController } from './category.controller';
-import { CategoryService } from './category.service';
 
 @Module({
   imports: [],
-  controllers: [CategoryController],
-  providers: [CategoryService],
+  controllers: [],
+  providers: [],
 })
 export class CategoryModule {}
