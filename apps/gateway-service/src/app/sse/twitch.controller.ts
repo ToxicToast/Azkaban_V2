16ff91e860ea75a9852344bff1243edf0dda5b48
Thomas Kortyka <t.kortyka@gmx.de>
2023-11-25 03:50:06 +0100

    chore: Example with onEvent and Subject to SSE

--- apps/gateway-service/src/app/sse/twitch.controller.ts
+++ apps/gateway-service/src/app/sse/twitch.controller.ts
@@ -1,17 +1,32 @@
 import { Controller, Sse } from '@nestjs/common';
-import { fromEvent, map, Observable } from 'rxjs';
-import { EventEmitter2 } from '@nestjs/event-emitter';
+import { fromEvent, map, Observable, Subject } from 'rxjs';
+import { EventEmitter2, OnEvent } from '@nestjs/event-emitter';
 
 @Controller('twitch')
 export class TwitchController {
+  private readonly events$ = new Subject<MessageEvent>();
+
   constructor(private readonly eventEmitter: EventEmitter2) {}
 
   @Sse()
   onEvents(): Observable<MessageEvent> {
-    return fromEvent(this.eventEmitter, 'twitch.event').pipe(
+    return this.events$.asObservable();
+    /*return fromEvent(this.eventEmitter, 'twitch.event').pipe(
       map((data) => {
         return new MessageEvent('twitch.event', { data });
       })
-    );
+    );*/
+  }
+
+  @OnEvent('twitch.join')
+  onJoin(payload: { channel: string; username: string }): void {
+    const message = new MessageEvent('twitch.join', {
+      data: {
+        channel: payload.channel,
+        username: payload.username,
+        type: 'join',
+      },
+    });
+    this.events$.next(message);
   }
 }
