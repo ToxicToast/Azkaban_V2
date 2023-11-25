82eb31b4ad1843608a1c5b42ccc57b0e7c7c6616
Thomas Kortyka <t.kortyka@gmx.de>
2023-11-25 22:55:52 +0100

    chore: Remove unnecessary code

--- apps/gateway-service/src/app/sse/twitch.controller.ts
+++ apps/gateway-service/src/app/sse/twitch.controller.ts
@@ -1,21 +1,14 @@
 import { Controller, Sse } from '@nestjs/common';
-import { fromEvent, map, Observable, Subject } from 'rxjs';
-import { EventEmitter2, OnEvent } from '@nestjs/event-emitter';
+import { Observable, Subject } from 'rxjs';
+import { OnEvent } from '@nestjs/event-emitter';
 
 @Controller('twitch')
 export class TwitchController {
   private readonly events$ = new Subject<MessageEvent>();
 
-  constructor(private readonly eventEmitter: EventEmitter2) {}
-
   @Sse()
   onEvents(): Observable<MessageEvent> {
     return this.events$.asObservable();
-    /*return fromEvent(this.eventEmitter, 'twitch.event').pipe(
-      map((data) => {
-        return new MessageEvent('twitch.event', { data });
-      })
-    );*/
   }
 
   @OnEvent('twitch.join')
@@ -25,6 +18,35 @@ export class TwitchController {
         channel: payload.channel,
         username: payload.username,
         type: 'join',
+      },
+    });
+    this.events$.next(message);
+  }
+
+  @OnEvent('twitch.part')
+  onPart(payload: { channel: string; username: string }): void {
+    const message = new MessageEvent('twitch.part', {
+      data: {
+        channel: payload.channel,
+        username: payload.username,
+        type: 'part',
+      },
+    });
+    this.events$.next(message);
+  }
+
+  @OnEvent('twitch.message')
+  onMessage(payload: {
+    channel: string;
+    username: string;
+    message: string;
+  }): void {
+    const message = new MessageEvent('twitch.message', {
+      data: {
+        channel: payload.channel,
+        username: payload.username,
+        message: payload.message,
+        type: 'message',
       },
     });
     this.events$.next(message);
