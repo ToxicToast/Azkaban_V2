b68c18d422fc9d2c55028c1e6da2a5206f748ccc
Thomas Kortyka <info@toxictoast.de>
2023-12-04 10:26:44 +0100

    chore: Change Notification SSE

--- apps/gateway-service/src/app/sse/notification.controller.ts
+++ apps/gateway-service/src/app/sse/notification.controller.ts
@@ -1,17 +1,21 @@
 import { Controller, Sse } from '@nestjs/common';
-import { fromEvent, map, Observable } from 'rxjs';
-import { EventEmitter2 } from '@nestjs/event-emitter';
+import { Observable, Subject } from 'rxjs';
+import { OnEvent } from '@nestjs/event-emitter';
 
 @Controller('notification')
 export class NotificationController {
-  constructor(private readonly eventEmitter: EventEmitter2) {}
+  private readonly events$ = new Subject<MessageEvent>();
 
   @Sse()
   onEvents(): Observable<MessageEvent> {
-    return fromEvent(this.eventEmitter, 'notification.event').pipe(
-      map((data) => {
-        return new MessageEvent('notification.event', { data });
-      })
-    );
+    return this.events$.asObservable();
+  }
+
+  @OnEvent('notification.event')
+  onEvent(payload): void {
+    const message = new MessageEvent('notification.event', {
+      data: payload,
+    });
+    this.events$.next(message);
   }
 }
