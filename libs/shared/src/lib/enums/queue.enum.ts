27b488bba610311b1fe8990c74d332e39ef0ca53
Thomas Kortyka <info@toxictoast.de>
2024-02-13 16:43:50 +0100

    chore: add tags to project + dockerfile + package scripts + queue name

--- libs/shared/src/lib/enums/queue.enum.ts
+++ libs/shared/src/lib/enums/queue.enum.ts
@@ -3,6 +3,7 @@ export enum Queues {
   INVENTORY_CATEGORIES = 'inventory_category_queue',
   INVENTORY_COMPANIES = 'inventory_company_queue',
   INVENTORY_ITEMS = 'inventory_item_queue',
+  INVENTORY_LOCATION = 'inventory_location_queue',
   // Twitch
   TWITCH = 'twitch_queue',
 }
