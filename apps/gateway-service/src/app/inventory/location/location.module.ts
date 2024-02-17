b9cef63fa02709d2df50685a7a963d28f7f855be
Thomas Kortyka <info@toxictoast.de>
2024-02-17 01:20:57 +0100

    feat: add query type and fix old one

--- apps/gateway-service/src/app/inventory/location/location.module.ts
+++ apps/gateway-service/src/app/inventory/location/location.module.ts
@@ -2,10 +2,21 @@ import { Module } from '@nestjs/common';
 import { AuthGuard } from '../../../guard/auth.guard';
 import { GroupsGuard } from '../../../guard/groups.guard';
 import { JwtModule } from '@nestjs/jwt';
+import { ItemService } from '../item/item.service';
+import { ClientsModule } from '@nestjs/microservices';
+import { clientProvider, Queues } from '@azkaban/shared';
 
 @Module({
-  imports: [JwtModule],
+  imports: [
+    JwtModule,
+    ClientsModule.register([
+      {
+        name: 'LOCATION_SERVICE',
+        ...clientProvider(Queues.INVENTORY_LOCATION),
+      },
+    ]),
+  ],
   controllers: [],
-  providers: [AuthGuard, GroupsGuard],
+  providers: [AuthGuard, GroupsGuard, ItemService],
 })
 export class LocationModule {}
--- libs/shared/src/lib/enums/queue.enum.ts
+++ libs/shared/src/lib/enums/queue.enum.ts
@@ -5,6 +5,7 @@ export enum Queues {
   INVENTORY_ITEMS = 'inventory_item_queue',
   INVENTORY_LOCATION = 'inventory_location_queue',
   INVENTORY_SIZE = 'inventory_size_queue',
+  INVENTORY_TYPE = 'inventory_type_queue',
   // Twitch
   TWITCH = 'twitch_queue',
 }
