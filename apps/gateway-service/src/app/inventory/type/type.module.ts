6eb7f3dc418c827bdef890bd86b2d497a76f8998
Thomas Kortyka <info@toxictoast.de>
2024-02-19 10:42:36 +0100

    feat: add gateway type

--- apps/gateway-service/src/app/inventory/type/type.module.ts
+++ apps/gateway-service/src/app/inventory/type/type.module.ts
@@ -2,10 +2,22 @@ import { Module } from '@nestjs/common';
 import { AuthGuard } from '../../../guard/auth.guard';
 import { GroupsGuard } from '../../../guard/groups.guard';
 import { JwtModule } from '@nestjs/jwt';
+import { TypeService } from './type.service';
+import { ClientsModule } from '@nestjs/microservices';
+import { clientProvider, Queues } from '@azkaban/shared';
+import { TypeController } from './type.controller';
 
 @Module({
-  imports: [JwtModule],
-  controllers: [],
-  providers: [AuthGuard, GroupsGuard],
+  imports: [
+    JwtModule,
+    ClientsModule.register([
+      {
+        name: 'TYPE_SERVICE',
+        ...clientProvider(Queues.INVENTORY_TYPE),
+      },
+    ]),
+  ],
+  controllers: [TypeController],
+  providers: [AuthGuard, GroupsGuard, TypeService],
 })
 export class TypeModule {}
