ed303b53ff399c0331740324001d6112b31bd41a
Thomas Kortyka <info@toxictoast.de>
2024-02-19 10:08:25 +0100

    feat: add gateway size

--- apps/gateway-service/src/app/inventory/size/size.module.ts
+++ apps/gateway-service/src/app/inventory/size/size.module.ts
@@ -2,10 +2,22 @@ import { Module } from '@nestjs/common';
 import { AuthGuard } from '../../../guard/auth.guard';
 import { GroupsGuard } from '../../../guard/groups.guard';
 import { JwtModule } from '@nestjs/jwt';
+import { SizeService } from './size.service';
+import { SizeController } from './size.controller';
+import { ClientsModule } from '@nestjs/microservices';
+import { clientProvider, Queues } from '@azkaban/shared';
 
 @Module({
-  imports: [JwtModule],
-  controllers: [],
-  providers: [AuthGuard, GroupsGuard],
+  imports: [
+    JwtModule,
+    ClientsModule.register([
+      {
+        name: 'SIZE_SERVICE',
+        ...clientProvider(Queues.INVENTORY_SIZE),
+      },
+    ]),
+  ],
+  controllers: [SizeController],
+  providers: [AuthGuard, GroupsGuard, SizeService],
 })
 export class SizeModule {}
---
+++ apps/gateway-service/src/app/inventory/size/size.service.ts
@@ -0,0 +1,64 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientRMQ } from '@nestjs/microservices';
+import {
+  CreateSizeDto,
+  SizeDao,
+  UpdateSizeDto,
+} from '@azkaban/inventory-infrastructure';
+import { InventorySizeTopics, Nullable } from '@azkaban/shared';
+
+@Injectable()
+export class SizeService {
+  constructor(@Inject('SIZE_SERVICE') private readonly client: ClientRMQ) {}
+
+  async getSizes(): Promise<Array<SizeDao>> {
+    return await this.client
+      .send<Array<SizeDao>, object>(InventorySizeTopics.LIST, {})
+      .toPromise();
+  }
+
+  async getSizeById(id: string): Promise<Nullable<SizeDao>> {
+    return await this.client
+      .send<Nullable<SizeDao>, string>(InventorySizeTopics.ID, id)
+      .toPromise();
+  }
+
+  async createSize(data: CreateSizeDto): Promise<string> {
+    return await this.client
+      .send<string, CreateSizeDto>(InventorySizeTopics.CREATE, data)
+      .toPromise();
+  }
+
+  async updateSize(id: string, data: UpdateSizeDto): Promise<void> {
+    return await this.client
+      .send<
+        void,
+        { id: string; data: UpdateSizeDto }
+      >(InventorySizeTopics.UPDATE, { id, data })
+      .toPromise();
+  }
+
+  async deleteSize(id: string): Promise<void> {
+    return await this.client
+      .send<void, string>(InventorySizeTopics.DELETE, id)
+      .toPromise();
+  }
+
+  async restoreSize(id: string): Promise<void> {
+    return await this.client
+      .send<void, string>(InventorySizeTopics.RESTORE, id)
+      .toPromise();
+  }
+
+  async activateSize(id: string): Promise<void> {
+    return await this.client
+      .send<void, string>(InventorySizeTopics.ACTIVATE, id)
+      .toPromise();
+  }
+
+  async deactivateSize(id: string): Promise<void> {
+    return await this.client
+      .send<void, string>(InventorySizeTopics.DEACTIVATE, id)
+      .toPromise();
+  }
+}
