03e0f6c62df7559b2c11b0829e1dbb5fb07b9c9c
Thomas Kortyka <info@toxictoast.de>
2024-04-16 11:49:32 +0200

    chore: fix hawo id

--- apps/twitch-hook-service/src/middlewares/hawokai.middleware.ts
+++ apps/twitch-hook-service/src/middlewares/hawokai.middleware.ts
@@ -2,7 +2,7 @@ import { EventSubMiddleware } from '@twurple/eventsub-http';
 import { MiddlewareBuilder } from '../utils/middleware.builder';
 
 export class HawokaiMiddleware {
-  private readonly id = '104373712';
+  private readonly id = '451543189';
 
   constructor(private readonly middleware: EventSubMiddleware) {
     const builder = new MiddlewareBuilder(this.middleware, this.id);
